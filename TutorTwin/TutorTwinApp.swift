62b6e0f63cb705f0d3cb4859aba168777a7ba6eb
sukeina <smammar@umich.edu>
2026-05-16 23:54:58 -0400

    Initial Commit

---
+++ TutorTwin/TutorTwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorTwinApp.swift
+//  TutorTwin
+//
+//  Created by Sukeina Ammar on 5/16/26.
+//
+
+import SwiftUI
+
+@main
+struct TutorTwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
